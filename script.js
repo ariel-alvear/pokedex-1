3d877b1ede4e1a3e6302ce7cd56197cc2440a2ac
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-04-23 19:51:34 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// https://pokeapi.co/api/v2/pokemon/ url de primeros 20
+// "https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20" URL para los siguientes 20
+
+$(document).ready(function(){
+    function requestApi() {
+        $.ajax({
+            url: 'https://pokeapi.co/api/v2/pokemon/',
+            context: document.body,
+            method: 'GET',
+            success: function(response){ 
+                console.log(response)
+                }
+        })
