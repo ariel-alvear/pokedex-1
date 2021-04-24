dfbfee9b0cc21d658353db8de9e43a852027c90e
Sixto Guerra <sixto.guerra1982@gmail.com>
2021-04-24 11:33:04 -0400

    avances de sabado

--- script.js
+++ script.js
@@ -3,12 +3,12 @@
 
 $(document).ready(function(){
     function requestApi(pokemon) {
-        var respuesta 
+        var respuesta
         $.ajax({
             url: pokemon,
             context: document.body,
             method: 'GET',
-            success: function(response){ 
+            success: function(response){
                 var n = 1
                 respuesta = response
                 response.results.forEach(function(info){
@@ -16,30 +16,50 @@ $(document).ready(function(){
                     <div class="card mb-5 mt-5 pt-5 pb-5" style="width: 18rem;">
                     <div class="card-body">
                       <h1 class="card-title">${info.name}</h1>
-                      <a id='enlace-${n}' href="#" url="${info.url}" class="btn btn-primary">¡Quiero ver más de este pokémon!</a>
+                      <a id='enlace-${n}' href="#" url="${info.url}" class="btn btn-primary pokemodal">¡Quiero ver más de este pokémon!</a>
                     </div>
-                  </div> 
+                  </div>
                   </div>`
                   $('#info').append(details);
                   n = n + 1;
                 })
                 next_url = response.next
-                let nextAdress = `<a id='pokeparagraph' href="${response.next}">Ver los otros pokemones</a>` 
+                back_url = response.previous
+                if (back_url == null){
+                  $('#btn_back').hide()
+                } else {
+                  $('#btn_back').show()
+                }
+                let nextAdress = `<a id='pokeparagraph' href="${response.next}">Ver los otros pokemones</a>`
                 $('#next-button').append(nextAdress)
                 console.log(nextAdress)
+
+                $('.pokemodal').click(function(e){
+                  e.preventDefault();
+                  alert('levantar modal');
+                });
             }
+
         })
         console.log(respuesta)
     }
-    var next_url 
-     requestApi('https://pokeapi.co/api/v2/pokemon/');
+    var back_url
+    var next_url
+    requestApi('https://pokeapi.co/api/v2/pokemon/');
+
+
 
     function activeButton (){
-        $('#btn').click(function(e){
+        $('#btn').click(function(){
             $('#info').empty('.pokemon')
             $('#pokeparagraph').remove()
-            requestApi(next_url)             
+            requestApi(next_url)
+        });
+        $('#btn_back').click(function(){
+          $('#info').empty('.pokemon')
+          $('#pokeparagraph').remove()
+          requestApi(back_url)
         });
     };
     activeButton()
-});    
+});
