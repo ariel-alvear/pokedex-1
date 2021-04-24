d7e3851a2ce0929db7ff47402d5d967c0f4c8c02
Sixto Guerra <sixto.guerra1982@gmail.com>
2021-04-23 20:11:05 -0400

    listar pokemones

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ $(document).ready(function(){
             context: document.body,
             method: 'GET',
             success: function(response){ 
-                console.log(response)
-                }
+                var n = 1
+                response.results.forEach(function(info){
+                    let details = `<div class='container col-md-4 '>
+                    <div class="card mb-5 mt-5 pt-5 pb-5" style="width: 18rem;">
+                    <img id='current-${n}' class='card-img-top' alt='error' width='100' height='100'>
+                    <div class="card-body">
+                      <h1 class="card-title">${info.name}</h1>
+                      <a id='enlace-${n}' href="#" url="${info.url}" class="btn btn-primary">¡Quiero ver más de este pokémon!</a>
+                    </div>
+                  </div> 
+                  </div>`
+                  $('#info').append(details);
+                  n = n + 1;
+                })
+            }
         })
+    }
+    requestApi();
+})    
